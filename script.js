7da8d245caaf90d2fba1df383e880d5748ee054c
TommyN987 <tomas.nagy.tn@gmail.com>
2022-03-18 17:34:38 +0100

    add much functionality

--- script.js
+++ script.js
@@ -1,41 +1,109 @@
-const library = [];
+/* ************************************
+** Constructor
+************************************** */
 
-class Book {
-  constructor(title, author, pages, haveRead) {
+class Movie {
+  constructor(title, director, year, minutes, haveSeen = true) {
     this.title = title
-    this.author = author
-    this.pages = pages
-    this.haveRead = haveRead
-    this.info = function () {
-      let readStatus;
-      if (this.haveRead) {
-        readStatus = 'has been read';
+    this.director = director
+    this.year = year
+    this.minutes = minutes
+    this.haveSeen = haveSeen
+    this.id = title
+    this.info = () => {
+      let seenStatus;
+      if (this.haveSeen) {
+        seenStatus = 'seen it';
       } else
-        readStatus = 'not read yet';
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`
+        seenStatus = 'not seen it yet';
+      return `This is ${this.title} (${year}) by ${this.director}, it's ${this.minutes} minutes long, and I have ${seenStatus}`
+    }
+    this.remove = () => {
+      library.splice(library.indexOf(this), 1)
     }
   }
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
-const theLittlePrince = new Book('The Little Prince', 'Antoine de Saint-Exupery', 123, true);
+/* ************************************
+** DOM elements
+************************************** */
 
-// console.log(theHobbit);
-// console.log(theHobbit.info());
+const library = [];
+const container = document.getElementById('container');
+const modal = document.getElementById('modal');
+const movieTitle = document.getElementById('title');
+const movieDirector = document.getElementById('director');
+const movieYear = document.getElementById('year');
+const duration = document.getElementById('duration');
+const statusSeen = document.getElementById('seen');
+const statusNotSeen = document.getElementById('not-seen');
+const btnSubmitMovie = document.getElementById('btn-submit');
 
-const addBook = (book) => {
-  library.push(book);
+/* ************************************
+** Functions
+************************************** */
+
+const checkSeen = () => {
+  return statusNotSeen.checked ? false : true;
 }
 
-addBook(theHobbit);
-addBook(theLittlePrince);
+const createMovie = () => {
+  const addition = new Movie(movieTitle.value, movieDirector.value, movieYear.value, duration.value, checkSeen());
+  return addition;
+}
 
-
+const renderMovie = (movie) => {
+  container.insertAdjacentHTML("afterbegin", `
+    <div class="col-md-6 col-lg-3 card bg-dark bg-gradient border-warning" id="${movie.id}">
+      <div class="card-header pt-4">
+        <h5 class="card-title">${movie.title}</h5>
+        <h6 class="card-subtitle mb-2">${movie.director}</h6>
+        <h6 class="card-subtitle mb-2">${movie.year}</h6>
+      </div>
+      <div class="card-body">
+        <p class="card-text">${movie.info()}</p>
+        <div class="d-flex justify-content-end">
+          <button type="button" class="btn btn-success mx-1"></button>
+          <button type="button" class="btn btn-danger mx-1">Delete</button>
+        </div>
+      </div>
+    </div>`
+  )
+  const btnRemove = document.querySelector('.btn-danger');
+  btnRemove.addEventListener('click', () => { 
+    movie.remove();
+    (document.getElementById(`${movie.title}`)).remove();
+  });
+}
 
 const renderLibrary = () => {
   for (i = 0; i < library.length; i++) {
-    console.log(library[i]);
-  }
+    renderMovie(library[i]);
+}}
+
+const submitMovie = () => {
+  library.unshift(createMovie());
+  renderMovie(library[0]);
+  const inputs = document.querySelectorAll('input');
+  inputs.forEach(input => {
+    input.value = '';
+    input.checked = false;
+  })
 }
 
+/* ************************************
+** Default library setup
+************************************** */
+
+const enterTheVoid = new Movie ('Enter the Void', 'Gaspar Noé', 2009, 161);
+const oldboy = new Movie ('Oldboy', 'Park Chan-wook', 2003, 120);
+
+library.push(enterTheVoid, oldboy);
+
 renderLibrary();
+
+/* ************************************
+** Event listeners
+************************************** */
+
+btnSubmitMovie.addEventListener('click', submitMovie);
